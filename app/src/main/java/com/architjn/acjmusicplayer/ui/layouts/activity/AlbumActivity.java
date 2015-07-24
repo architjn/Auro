45b9d7d762191f131cb203801afb37819744a8f9
architjn <architjn93@gmail.com>
2015-07-24 20:44:02 +0530

    Added transparent Navigation Bar

--- app/src/main/java/com/architjn/acjmusicplayer/ui/layouts/activity/AlbumActivity.java
+++ app/src/main/java/com/architjn/acjmusicplayer/ui/layouts/activity/AlbumActivity.java
@@ -55,15 +55,20 @@ public class AlbumActivity extends AppCompatActivity {
         if (settingsPref.getBoolean("pref_album_status_trans", true)) {
             if (Build.VERSION.SDK_INT >= 21)
                 getWindow().setStatusBarColor(Color.TRANSPARENT);
-//            getWindow().getDecorView().setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         } else {
             if (Build.VERSION.SDK_INT >= 21) {
                 Window w = getWindow();
                 w.setFlags(
                         WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                         WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            }
+        }
+
+        if (settingsPref.getBoolean("pref_nav_trans", false)) {
+            if (Build.VERSION.SDK_INT >= 21) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+                getWindow().setNavigationBarColor(Color.TRANSPARENT);
             }
         }
 
