25a281f31b0a4f830f134f5499d0701cebba963b
architjn <architjn93@gmail.com>
2015-08-31 23:20:52 +0530

    added Home activity

--- app/src/main/java/com/architjn/acjmusicplayer/ui/layouts/activity/HomeActivity.java
+++ app/src/main/java/com/architjn/acjmusicplayer/ui/layouts/activity/HomeActivity.java
@@ -3,7 +3,6 @@ package com.architjn.acjmusicplayer.ui.layouts.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -14,7 +13,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -22,32 +20,39 @@ import com.architjn.acjmusicplayer.R;
 import com.architjn.acjmusicplayer.elements.adapters.ViewPagerAdapter;
 import com.architjn.acjmusicplayer.service.MusicService;
 import com.architjn.acjmusicplayer.ui.layouts.activity.settings.Settings;
-import com.architjn.acjmusicplayer.ui.layouts.fragments.AlbumsFragment;
-import com.architjn.acjmusicplayer.ui.layouts.fragments.ArtistsFragment;
-import com.architjn.acjmusicplayer.ui.layouts.fragments.GenresFragment;
-import com.architjn.acjmusicplayer.ui.layouts.fragments.SongsFragment;
+import com.architjn.acjmusicplayer.ui.layouts.fragments.HomeFragment;
+import com.architjn.acjmusicplayer.ui.layouts.fragments.PlaylistFragment;
+
+/**
+ * Created by architjn on 31/08/15.
+ */
+public class HomeActivity extends AppCompatActivity {
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private SharedPreferences settingsPref;
     private FloatingActionButton fab;
-    private DrawerLayout drawerLayout;
-    private Toolbar toolbar;
     private ViewPager viewPager;
+    private Toolbar toolbar;
+    private SharedPreferences settingsPref;
     private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        overridePendingTransition(0, 0);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        setSupportActionBar(toolbar);
-
+        setContentView(R.layout.activity_home);
         init();
+        setSupportActionBar(toolbar);
         setDrawer();
+        setupViewPager();
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.home_tablayout);
+        if (settingsPref.getBoolean("pref_extend_tabs", false))
+            tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        else
+            tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setupWithViewPager(viewPager);
 
+        Intent i = new Intent(HomeActivity.this, MusicService.class);
+        startService(i);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,41 +61,28 @@ public class MainActivity extends AppCompatActivity {
                 sendBroadcast(requestSongDetials);
             }
         });
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            getWindow().setExitTransition(new Explode());
-        }
-
-        setupViewPager(viewPager);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
-        if (settingsPref.getBoolean("pref_extend_tabs", false))
-            tabLayout.setTabMode(TabLayout.MODE_FIXED);
-        else
-            tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
-        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void init() {
-        fab = (FloatingActionButton) findViewById(R.id.fab_main);
-        viewPager = (ViewPager) findViewById(R.id.main_viewPager);
-        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        fab = (FloatingActionButton) findViewById(R.id.fab_home);
+        viewPager = (ViewPager) findViewById(R.id.home_viewPager);
+        toolbar = (Toolbar) findViewById(R.id.home_toolbar);
         settingsPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawerlayout);
-        navigationView = (NavigationView) findViewById(R.id.main_navigationview);
+        drawerLayout = (DrawerLayout) findViewById(R.id.home_drawerlayout);
+        navigationView = (NavigationView) findViewById(R.id.home_navigationview);
     }
 
-    private void setupViewPager(ViewPager viewPager) {
+    private void setupViewPager() {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFrag(new SongsFragment(), getResources().getString(R.string.songs));
-        adapter.addFrag(new AlbumsFragment(), getResources().getString(R.string.album));
-        adapter.addFrag(new ArtistsFragment(), getResources().getString(R.string.artist));
-        adapter.addFrag(new GenresFragment(), getResources().getString(R.string.genres));
+        adapter.addFrag(new HomeFragment(), getResources().getString(R.string.home));
+        adapter.addFrag(new PlaylistFragment(), getResources().getString(R.string.playlist));
         viewPager.setAdapter(adapter);
-        viewPager.setCurrentItem(getIntent().getIntExtra("pos", 2) - 2);
+        viewPager.setCurrentItem(0);
     }
 
     public void setDrawer() {
         drawerLayout.setStatusBarBackgroundColor(Color.TRANSPARENT);
+        navigationView.getMenu().getItem(0).setChecked(true);
         if (toolbar != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             toolbar.setNavigationIcon(R.drawable.ic_drawer);
@@ -101,39 +93,49 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-        navigationView.getMenu().getItem(getIntent().getIntExtra("pos", 2)).setChecked(true);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.navigation_home:
-                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
-                        finish();
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                        break;
-                    case R.id.navigation_playlist:
-                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
-                        finish();
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                        break;
-                    case R.id.navigation_songs:
                         viewPager.setCurrentItem(0);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
-                    case R.id.navigation_albums:
+                    case R.id.navigation_playlist:
                         viewPager.setCurrentItem(1);
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
+                    case R.id.navigation_songs:
+                        Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                        i.putExtra("pos", 2);
+                        startActivity(i);
+                        finish();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.navigation_albums:
+                        Intent a = new Intent(HomeActivity.this, MainActivity.class);
+                        a.putExtra("pos", 3);
+                        startActivity(a);
+                        finish();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
                     case R.id.navigation_artists:
-                        viewPager.setCurrentItem(2);
+                        Intent b = new Intent(HomeActivity.this, MainActivity.class);
+                        b.putExtra("pos", 4);
+                        startActivity(b);
+                        finish();
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
                     case R.id.navigation_genres:
-                        viewPager.setCurrentItem(3);
+                        Intent c = new Intent(HomeActivity.this, MainActivity.class);
+                        c.putExtra("pos", 5);
+                        startActivity(c);
+                        finish();
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
                     case R.id.navigation_sub_item_2:
-                        startActivity(new Intent(MainActivity.this, Settings.class));
+                        startActivity(new Intent(HomeActivity.this, Settings.class));
+                        finish();
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
                 }
@@ -148,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageSelected(int position) {
-                navigationView.getMenu().getItem(position + 2).setChecked(true);
+                navigationView.getMenu().getItem(position).setChecked(true);
             }
 
             @Override
@@ -156,31 +158,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (fab.getVisibility() != View.VISIBLE)
-            fab.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(MainActivity.this, Settings.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
---
+++ app/src/main/java/com/architjn/acjmusicplayer/ui/layouts/fragments/PlaylistFragment.java
@@ -0,0 +1,23 @@
+package com.architjn.acjmusicplayer.ui.layouts.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.architjn.acjmusicplayer.R;
+
+/**
+ * Created by architjn on 31/08/15.
+ */
+public class PlaylistFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.albums_fragment, container, false);
+        return v;
+    }
+
+}
